051a78ef8ad47e19f7c60b6a62264806486912b5
bijay-develops <bijaybk307@gmail.com>
2026-03-02 18:30:59 +0545

    feat: implement FetchWorker for processing items with HTTP requests

---
+++ crawler/internal/pipeline/fetch.go
@@ -0,0 +1,48 @@
+package pipeline
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"crawler/internal/crawler"
+)
+
+func NewHTTPClient(timeout time.Duration) *http.Client {
+	return &http.Client{Timeout: timeout}
+}
+
+func FetchWorker(
+	ctx context.Context,
+	client *http.Client,
+	limiter *DomainLimiter,
+	in <-chan crawler.Item,
+	out chan<- crawler.Item,
+) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case item, ok := <-in:
+			if !ok {
+				return
+			}
+
+			limiter.Wait(item.URL.Host)
+
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, item.URL.String(), nil)
+			if err != nil {
+				continue
+			}
+
+			item.Response = resp
+
+			select {
+			case out <- item:
+			case <-ctx.Done():
+				resp.Body.Close()
+				return
+			}
+		}
+	}
+}
